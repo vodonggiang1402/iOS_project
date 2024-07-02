10ff5001b4097f2cd3d51fb30fc9f021a280fb9b
giang.vo <vodonggiang@Vos-MacBook-Pro.local>
2024-07-02 13:12:33 +0700

    push code

---
+++ TMadeCrochet/TMadeCrochet/CustomView/CollectionView/BaseCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BaseCollectionViewCell.swift
+//  Probit
+//
+//  Created by Nguyen Quang on 24/08/2022.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    func setupCell(object: Any) {
+    }
+}
