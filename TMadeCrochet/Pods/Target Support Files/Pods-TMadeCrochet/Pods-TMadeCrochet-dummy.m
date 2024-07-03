c25656065191ab81328e8dca7f1e50e7e9871da2
giang.vo <vodonggiang@Vos-MacBook-Pro.local>
2024-07-03 09:59:52 +0700

    remove pod client

--- TMadeCrochet/Pods/Target Support Files/Pods-TMadeCrochet/Pods-TMadeCrochet-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TMadeCrochet : NSObject
-@end
-@implementation PodsDummy_Pods_TMadeCrochet
-@end
--- TMadeCrochet/Pods/Target Support Files/Pods-TMadeCrochetTests/Pods-TMadeCrochetTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TMadeCrochetTests : NSObject
-@end
-@implementation PodsDummy_Pods_TMadeCrochetTests
-@end
