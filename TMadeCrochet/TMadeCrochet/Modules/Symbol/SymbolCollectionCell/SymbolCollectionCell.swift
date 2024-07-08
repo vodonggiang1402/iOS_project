0d396b5297822fbb465b7b10a8b79542e47328bd
giang.vo <giang.vo@probit.com>
2024-07-08 11:27:08 +0700

    push code

--- TMadeCrochet/TMadeCrochet/Modules/Symbol/SymbolCollectionCell/SymbolCollectionCell.swift
+++ TMadeCrochet/TMadeCrochet/Modules/Symbol/SymbolCollectionCell/SymbolCollectionCell.swift
@@ -9,14 +9,19 @@ import Foundation
 import UIKit
 
 class SymbolCollectionCell: BaseCollectionViewCell {
+    @IBOutlet weak var containView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.containView.layer.cornerRadius = 8.0
+        self.containView.layer.masksToBounds = true
     }
     
     override func setupCell(object: Any) {
         guard let model = object as? Symbol else { return }
         self.titleLabel.text = model.symbolName
+        self.imageView.image = UIImage.init(named: model.iconName ?? "")
     }
 }
