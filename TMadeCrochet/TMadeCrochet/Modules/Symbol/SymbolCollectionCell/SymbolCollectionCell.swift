87bf7041358d12aed2223a7f23ae26184234cf72
Ly Nguyen Xuan Thao <>
2024-07-09 12:38:05 +0700

    test

--- TMadeCrochet/TMadeCrochet/Modules/Symbol/SymbolCollectionCell/SymbolCollectionCell.swift
+++ TMadeCrochet/TMadeCrochet/Modules/Symbol/SymbolCollectionCell/SymbolCollectionCell.swift
@@ -11,32 +11,18 @@ import UIKit
 class SymbolCollectionCell: BaseCollectionViewCell {
     @IBOutlet weak var containView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
-<<<<<<< HEAD
-    @IBOutlet weak var iconImageView: UIImageView!
-    @IBOutlet weak var containView: UIView!
-     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        self.containView.backgroundColor = UIColor.color_989898_868686
-        self.containView.layer.cornerRadius = 10
-=======
     @IBOutlet weak var imageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         self.containView.layer.cornerRadius = 8.0
->>>>>>> 8c2e4b557964b03a8f97dc11eb6a6d3bf10bda14
         self.containView.layer.masksToBounds = true
     }
     
     override func setupCell(object: Any) {
         guard let model = object as? Symbol else { return }
         self.titleLabel.text = model.symbolName
-<<<<<<< HEAD
-        self.iconImageView.image = UIImage.init(named: model.iconName ?? "")
-=======
         self.imageView.image = UIImage.init(named: model.iconName ?? "")
         self.containView.backgroundColor = UIColor(hexString: model.backgroundColor ?? "")
->>>>>>> 8c2e4b557964b03a8f97dc11eb6a6d3bf10bda14
     }
 }
