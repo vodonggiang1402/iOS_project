1e9bda1e986344fb954e78e63e4f08c342645704
Ly Nguyen Xuan Thao <>
2024-07-09 12:25:34 +0700

    no message

--- TMadeCrochet/TMadeCrochet/Modules/Symbol/SymbolCollectionCell/SymbolCollectionCell.swift
+++ TMadeCrochet/TMadeCrochet/Modules/Symbol/SymbolCollectionCell/SymbolCollectionCell.swift
@@ -10,13 +10,19 @@ import UIKit
 
 class SymbolCollectionCell: BaseCollectionViewCell {
     @IBOutlet weak var titleLabel: UILabel!
-    
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var containView: UIView!
+     
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.containView.backgroundColor = UIColor.color_989898_868686
+        self.containView.layer.cornerRadius = 10
+        self.containView.layer.masksToBounds = true
     }
     
     override func setupCell(object: Any) {
         guard let model = object as? Symbol else { return }
         self.titleLabel.text = model.symbolName
+        self.iconImageView.image = UIImage.init(named: model.iconName ?? "")
     }
 }
