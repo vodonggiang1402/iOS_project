1cb13537d262b4e96dae475b8b250129c866405b
Ly Nguyen Xuan Thao <>
2024-07-25 23:57:13 +0700

    push code

--- TMadeCrochet/TMadeCrochet/Modules/Tabbar/TabbarViewController.swift
+++ TMadeCrochet/TMadeCrochet/Modules/Tabbar/TabbarViewController.swift
@@ -146,7 +146,7 @@ class TabbarViewController: UITabBarController, AppOpenAdManagerDelegate {
         tabbarItem.setTitleTextAttributes(selectedAttributes, for: .selected)
         
         tabbarItem.imageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -2, right: 0)
-        tabbarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: 4)
+        tabbarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: 5)
         
         viewController.tabBarItem = tabbarItem
     }
@@ -218,7 +218,7 @@ extension TabbarViewController {
         tabbarItem.setTitleTextAttributes(selectedAttributes, for: .selected)
         
         tabbarItem.imageInsets = UIEdgeInsets(top: 2, left: 0, bottom: -2, right: 0)
-        tabbarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: 4)
+        tabbarItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: 5)
         
         viewController.tabBarItem = tabbarItem
         let navigation = UINavigationController(rootViewController: viewController)
