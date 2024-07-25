2210e6b3b614aa09512f4404cfbaf979b4d3bb83
Ly Nguyen Xuan Thao <>
2024-07-25 13:58:53 +0700

    push code

--- TMadeCrochet/TMadeCrochet/Modules/Setting/Policy/PolicyViewController.swift
+++ TMadeCrochet/TMadeCrochet/Modules/Setting/Policy/PolicyViewController.swift
@@ -9,12 +9,25 @@ import Foundation
 import UIKit
 
 class PolicyViewController: BaseViewController {
+    @IBOutlet weak var textView: UITextView!
     var presenter: ViewToPresenterPolicyProtocol?
     
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupNavigationBar(title: "policy_screen_header_title".Localizable(), isShowLeft: true)
+        self.loadFile()
+    }
+    
+    func loadFile() {
+        if let rtfPath = Bundle.main.url(forResource: "policy", withExtension: "rtf") {
+           do {
+               let attributedStringWithRtf: NSAttributedString = try NSAttributedString(url: rtfPath, options: [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.rtf], documentAttributes: nil)
+               self.textView.attributedText = attributedStringWithRtf
+           } catch let error {
+               print("Got an error \(error)")
+           }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
