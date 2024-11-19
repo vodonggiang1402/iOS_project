886375a81b270a4fa226fe98816f488c77e5b8ab
Ly Nguyen Xuan Thao <>
2024-11-19 12:45:48 +0700

    push code

--- TMadeCrochet/TMadeCrochet/Modules/Count/CountFooterView/CountFooterView.swift
+++ TMadeCrochet/TMadeCrochet/Modules/Count/CountFooterView/CountFooterView.swift
@@ -25,7 +25,7 @@ class CountFooterView: BaseCollectionReusableView {
     }
     
     override func setupView(text: String, imageStr: String) {
-        self.addButton.setTitle("add_more_text".Localizable(), for: .normal)
+        self.addButton.setTitle("", for: .normal)
         self.titleLabel.text = text
         self.imageView.image = UIImage.init(named: imageStr)
     }
